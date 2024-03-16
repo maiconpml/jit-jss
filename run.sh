c7c7925a3296778919621417ede74cb68d9158ad
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2024-03-16 15:07:01 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-yourfilenames=`ls ./$1`
-echo path lowerb makes penalties earlPenal tarPenalties
-for eachfile in $yourfilenames 
-do
-#echo $eachfile	
-./build/main $1/$eachfile lalala --searchType tabuSearch
-done
-
