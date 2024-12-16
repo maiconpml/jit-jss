df328e750d944776e47c34f5acb79a8182eae3dc
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-12-16 18:13:58 -0300

    feat: create cplex solver for JIT-JSS

--- State.cpp
+++ State.cpp
@@ -1265,11 +1265,10 @@ void heapSort(vector<unsigned>& arr, vector<unsigned>& dists) {
 	}
 }
 
-/* Schedule operations relaxing machine precedence of operations in relaxBlock*/
-void State::schedulerCplexRelax(vector<unsigned>& relaxBlock) {
+void State::cplexSolve(const unsigned maxSecs) {
 
-	vector<unsigned> auxJobs = inst.roots;
-	vector<vector<unsigned>> machOrder(inst.M, vector<unsigned>(0));
+	//vector<unsigned> auxJobs = inst.roots;
+	//vector<vector<unsigned>> machOrder(inst.M, vector<unsigned>(0));
 	vector<unsigned> dists;
 
 	dists.clear();
@@ -1285,12 +1284,9 @@ void State::schedulerCplexRelax(vector<unsigned>& relaxBlock) {
 			/* Objective*/
 			objExpr += (IloMax(inst.deadlines[i] - completionTimes[i - 1], 0) * inst.earlPenalties[i]) + (IloMax(completionTimes[i - 1] - inst.deadlines[i], 0) * inst.tardPenalties[i]);
 
-			/* Start time of a job's first operation must be greater than zero*/
-			if (!_job[i]) jitModel.add(completionTimes[i - 1] - inst.P[i] >= 0);
-
 			/* Job precedence constraints*/
-			if (job[i]) {
-				jitModel.add(completionTimes[i - 1] <= completionTimes[job[i] - 1] - inst.P[job[i]]);
+			if (inst.next[i].size()) {
+				jitModel.add(completionTimes[i - 1] <= completionTimes[inst.next[i][0] - 1] - inst.P[inst.next[i][0]]);
 			}
 
 			/* Penalties constraints*/
@@ -1300,53 +1296,24 @@ void State::schedulerCplexRelax(vector<unsigned>& relaxBlock) {
 			jitModel.add(IloMax(completionTimes[i - 1] - inst.deadlines[i], 0) >= 0);*/
 		}
 
-		vector<unsigned> machHeads(inst.M, 0);
-
-		/* Find machine heads*/
-		for (unsigned i = 1; i < inst.O; ++i) {
-			if (!_mach[i]) {
-				machHeads[inst.operToM[i]] = i;
-			}
+		for (unsigned i = 0; i < inst.J; ++i) {
+			jitModel.add(completionTimes[inst.roots[i] - 1] - inst.P[inst.roots[i]] >= 0);
 		}
 
-		/* Add or constraints between relaxed operations*/
-		for (unsigned i = 0; i < relaxBlock.size(); ++i) {
-			for (unsigned j = i + 1; j < relaxBlock.size(); ++j) {
-				jitModel.add(completionTimes[relaxBlock[i] - 1] <= completionTimes[relaxBlock[j] - 1] - inst.P[relaxBlock[j]] || completionTimes[relaxBlock[j] - 1] <= completionTimes[relaxBlock[i] - 1] - inst.P[relaxBlock[i]]);
-			}
-		}
-
-		/* For each machine, set machine precendence constraints*/
 		for (unsigned i = 0; i < inst.M; ++i) {
-			unsigned auxOp = machHeads[i];
-			while (mach[auxOp]) {
-				/* If current machine is the relaxed machine, set machine precedence constraints between non-relaxed and relaxed operations*/
-				if (relaxBlock.size() && i == inst.operToM[relaxBlock[0]]) {
-					/* If the next operations is a relaxed operation, set all precendence constraints between current and relaxed operations*/
-					if (mach[auxOp] == relaxBlock[0]) {
-						for (unsigned j = 0; j < relaxBlock.size(); ++j) {
-							jitModel.add(completionTimes[auxOp - 1] <= completionTimes[relaxBlock[j] - 1] - inst.P[relaxBlock[j]]);
-						}
-						auxOp = mach[relaxBlock[relaxBlock.size() - 1]];
-						continue;
-					}
-					/* If current operation is directly after relaxed operations, set all precedence constraints between relaxed and current operations*/
-					else if (_mach[auxOp] == relaxBlock[relaxBlock.size() - 1]) {
-						for (unsigned j = 0; j < relaxBlock.size(); ++j) {
-							jitModel.add(completionTimes[relaxBlock[j] - 1] <= completionTimes[auxOp - 1] - inst.P[auxOp]);
-						}
-					}
+			for (unsigned j = 0; j < inst.J; ++j) {
+				for (unsigned k = j + 1; k < inst.J; ++k) {
+					jitModel.add(completionTimes[inst.machOpers[i][j] - 1] <= completionTimes[inst.machOpers[i][k] - 1] - inst.P[inst.machOpers[i][k]] || completionTimes[inst.machOpers[i][k] - 1] <= completionTimes[inst.machOpers[i][j] - 1] - inst.P[inst.machOpers[i][j]]);
 				}
-				jitModel.add(completionTimes[auxOp - 1] <= completionTimes[mach[auxOp] - 1] - inst.P[mach[auxOp]]);
-				auxOp = mach[auxOp];
 			}
 		}
-
 
 		jitModel.add(IloMinimize(jitEnv, objExpr));
 
 		IloCplex jitCplex(jitEnv);
-		jitCplex.setParam(IloCplex::Param::TimeLimit, 1);
+		jitCplex.setParam(IloCplex::Param::TimeLimit, maxSecs);
+		jitCplex.setParam(IloCplex::Param::WorkMem, 8192);
+		jitCplex.setParam(IloCplex::Param::MIP::Strategy::File, 3);
 		jitCplex.setOut(jitEnv.getNullStream());
 
 		jitCplex.extract(jitModel);
@@ -1367,64 +1334,50 @@ void State::schedulerCplexRelax(vector<unsigned>& relaxBlock) {
 		cerr << "Error" << endl;
 	}
 
+	double penal = 0;
+
+	cout << endl;
+
+	for (unsigned i = 1; i < dists.size(); ++i) {
+		penal += (int)inst.deadlines[i] - (int)(dists[i] + inst.P[i]) >= 0 ? ((int)inst.deadlines[i] - (int)(dists[i] + inst.P[i])) * inst.earlPenalties[i] : ((int)(dists[i] + inst.P[i]) - (int)inst.deadlines[i]) * inst.tardPenalties[i];
+	}
+
+	cout << penal << endl <<  endl;
+
 	jitEnv.end();
 
-	makes = 0;
-	for (unsigned i = 1; i < inst.O; ++i) {
-		if (makes < dists[i] + inst.P[i]) {
-			makes = dists[i] + inst.P[i];
+	cout << penalties << endl << endl;
+
+	for (unsigned i = 0; i < inst.O; ++i) {
+		cout << dists[i] << " ";
+	}
+	cout << endl << endl;
+		
+	vector<unsigned> opersAux(inst.O - 1, 0);
+
+	for (unsigned i = 1; i <= inst.O; ++i) {
+		opersAux[i - 1] = i;
+	}
+
+	heapSort(opersAux, dists);
+
+	vector<vector<unsigned>> machOrderAux(inst.M, vector<unsigned>());
+
+	for (unsigned i = 0; i < inst.O-1; ++i) {
+		machOrderAux[inst.operToM[opersAux[i]]].push_back(opersAux[i]);
+	}
+
+	for (unsigned i = 0; i < inst.M; ++i) {
+		for (unsigned j = 0; j < inst.J - 1; ++j) {
+			mach[machOrderAux[i][j]] = machOrderAux[i][j + 1];
 		}
 	}
 
-	/* For each relaxed operation, verify if its order has changed*/
-	for (unsigned i = 0; i < relaxBlock.size() - 1; ++i) {
-		/* If the order of any relaxed operation has changed adjust order of relaxed machine*/
-		if (dists[relaxBlock[i]] > dists[relaxBlock[i + 1]]) {
-
-			priority_queue<pair<unsigned, unsigned>, vector<pair<unsigned, unsigned>>, greater<pair<unsigned, unsigned>>> q;
-
-			for (unsigned j = 0; j < relaxBlock.size(); ++j) {
-				q.push(pair<unsigned, unsigned>(dists[relaxBlock[j]], relaxBlock[j]));
-			}
-
-			unsigned prevOper = _mach[relaxBlock[0]];
-			unsigned nextOper = mach[relaxBlock[relaxBlock.size() - 1]];
-			unsigned curOper;
-			while (!q.empty()) {
-				curOper = q.top().second;
-				q.pop();
-				if (prevOper) mach[prevOper] = curOper;
-				_mach[curOper] = prevOper;
-				prevOper = curOper;
-			}
-			mach[curOper] = nextOper;
-			if (nextOper)	_mach[nextOper] = curOper;
-			break;
-		}
+	for (unsigned i = 0; i < inst.O; ++i) {
+		cout << mach[i] << " ";
 	}
 
-	inst.calcPenalties(dists, ePenalty, lPenalty, operPenalties);
-	penalties = ePenalty + lPenalty;
-	startTime = dists;
-}
-
-
-
-
-	}
-	jitEnv.end();
-
-	}
-
-
-
-	}
-
-		}
-	}
-
-	}
-
+	cout << endl;
 }
 
 		//@return: starts
